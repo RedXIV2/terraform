a8f2561820272fc9bb9e5fd5c137d15022258cec
Dave Hill <davidhill252@gmail.com>
2019-05-05 16:03:10 +0100

    first stab at terraform

---
+++ azure/aks-k8s/variables.tf
@@ -0,0 +1,40 @@
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "agent_count" {
+    default = 1
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "ca1-AKS-Cluster"
+}
+
+variable cluster_name {
+    default = "ca1-AKS-Cluster"
+}
+
+variable resource_group_name {
+    default = "ca1-resource-group"
+}
+
+variable location {
+    default = "North Europe"
+}
+
+variable log_analytics_workspace_name {
+    default = "testLogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+    default = "northeurope"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
