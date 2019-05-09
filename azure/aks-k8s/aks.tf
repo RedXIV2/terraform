063097df0668623dfdbd2683a20222b0dc2e746d
Dave Hill <davidhill252@gmail.com>
2019-05-09 21:48:48 +0100

    upping the azure AKS node types

--- azure/aks-k8s/aks.tf
+++ azure/aks-k8s/aks.tf
@@ -32,7 +32,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     agent_pool_profile {
         name            = "agentpool"
         count           = "${var.agent_count}"
-        vm_size         = "Standard_DS1_v2"
+        vm_size         = "Standard_DS2_v2"
         os_type         = "Linux"
         os_disk_size_gb = 30
     }
