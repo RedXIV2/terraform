5502edcf7d165e0154837519edd1d6875adc11a9
Dave Hill <davidhill252@gmail.com>
2019-10-14 02:54:03 +0100

    running tests with debug to find authentication issue

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -18,7 +18,7 @@ test_case_to_run=$1
 full_test="$(ls ${path_to_test}${test_case_to_run}*/* )"
 
 echo "$(date) Executing: ${full_test}" >> /myLogs.txt
-sudo -u ec2-user ansible-playbook ${full_test} >> /myLogs.txt 2>&1
+sudo -u ec2-user ansible-playbook -vvv ${full_test} >> /myLogs.txt 2>&1
 
 echo "$(date) ***Finished Test***" >> /myLogs.txt
 
