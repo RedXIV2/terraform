ba372e0cf374ac39645b6189d7a3d210489c6536
Dave Hill <davidhill252@gmail.com>
2019-11-28 02:49:21 +0000

    remove update step from webserver test

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -74,7 +74,7 @@ count = 10
         "sudo git clone https://github.com/RedXIV2/terraform.git /terraform",
         "sudo cp /terraform/scripts/addToFamily/addTo* /",
         "sudo chmod 777 /addTo*",
-        "curl --retry 5 -m 120 -X GET '${var.registrationAPI}?ipAddress=${self.private_ip}&cmTool=Ansible&testSuite=3'"
+        "curl --retry 5 -m 120 -X GET '${var.registrationAPI}?ipAddress=${self.private_ip}&cmTool=Ansible&testSuite=5'"
       ]
   }
 
